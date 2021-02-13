4f044bffc7d4095e14cc24f9471ec12448f87b2a
NIcolas <nicolas.huleux@mail.com>
2021-02-13 18:15:07 +0100

    commit

---
+++ 11. Solveurs linéaires/2. OPTIMISATIONS EN NOMBRES ENTIERS/2. Le problème du Sac à dos .py
@@ -0,0 +1,24 @@
+""" 
+
+Le problème du sac à dos .
+
+Maximiser la valeur des objets que je porte dans mon sac
+Sous contrainte de place maximale . 
+
+POurquoi est-ce un problème en nombres entiers ? 
+Par ce que on ne peut pas séparer un objet en 2, 
+on ne peut pas avoir 1,5 objet dans son sac , par exemple .
+
+Caractéristiques de mon problème : 
+
+Object          Valeur      Taille( En unités génériques .)
+
+
+
+
+Author: 
+Shanglun Wang
+https://www.toptal.com/algorithms/mixed-integer-programming
+https://www.toptal.com/resume/shanglun-sean-wang
+
+"""
